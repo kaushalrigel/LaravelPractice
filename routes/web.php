66568ddc074d48bd5bb462927413d43a6b32f9df
kaushalrigel <kaushal8154>
2023-01-25 11:59:47 +0530

    commit from branch

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { 
-    // my first laravel web route     
+    // my first laravel web route    999  88 
     return view('welcome');    
 });
