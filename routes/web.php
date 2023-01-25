3d69a88fc4a6f5cff57566a6a2a2f39a466b2665
kaushalrigel <kaushal8154>
2023-01-25 11:58:07 +0530

    2nd commit from master

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { 
-    // my first laravel web route     
+    // my first laravel web route 444 
     return view('welcome');    
 });
